3312e0fb409ebe5e8bf1a365922fdbf51ed8895d
Sithembiso Khumalo <sithembiso@lepsta.com>
2020-06-24 07:44:18 +0200

    Encode content with base64

--- .hooks/on_new_changeset/security.sh
+++ .hooks/on_new_changeset/security.sh
@@ -10,6 +10,7 @@ exit_status=$?
 out=$(echo "$out" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')
 out="${out%\"}"
 out="${out#\"}"
+out=$(echo "$out" | base64)
 
 if [ $exit_status -eq 1 ]; then
   echo "{\"id\": \"python-security\", \"output\": \"${out}\", \"score\": -1}"
